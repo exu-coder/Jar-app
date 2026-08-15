30d2fd6c69ef5aed969efd6655fdf494cb82847c
exu-coder <mdeitu3@gmail.com>
2026-08-15 23:44:50 +0600

    Upload j2me-engine/src/main/java/com/j2merunner/engine/midp/Command.kt via CLI Uploader

--- j2me-engine/src/main/java/com/j2merunner/engine/midp/Command.kt
+++ j2me-engine/src/main/java/com/j2merunner/engine/midp/Command.kt
@@ -4,30 +4,13 @@ package com.j2merunner.engine.midp
  * Bridge for javax.microedition.lcdui.Command
  */
 class Command(
-    private val cmdLabel: String,
-    private val cmdType: Int,
-    private val cmdPriority: Int
-) {
-    companion object {
-        const val SCREEN = 1
-        const val BACK = 2
-        const val CANCEL = 3
-        const val OK = 4
-        const val HELP = 5
-        const val STOP = 6
-        const val EXIT = 7
-        const val ITEM = 8
-    }
-
-    @JvmName("getLabelValue")
-    fun getLabel(): String = cmdLabel
-
-    @JvmName("getCommandTypeValue")
-    fun getCommandType(): Int = cmdType
-
-    @JvmName("getPriorityValue")
-    fun getPriority(): Int = cmdPriority
-}
+    @get:JvmName("getLabel")
+    val label: String,
+    @get:JvmName("getCommandType")
+    val commandType: Int,
+    @get:JvmName("getPriority")
+    val priority: Int
+)
 
 /**
  * Listener for command events
