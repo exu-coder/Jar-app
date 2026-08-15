e1857eb6d5896f8ad1a187fc72f6527c3a131e4c
exu-coder <mdeitu3@gmail.com>
2026-08-15 21:22:49 +0600

    Upload j2me-engine/src/test/java/com/j2merunner/engine/GraphicsTest.kt via CLI Uploader

---
+++ j2me-engine/src/test/java/com/j2merunner/engine/GraphicsTest.kt
@@ -0,0 +1,13 @@
+package com.j2merunner.engine
+
+import org.junit.Test
+import org.junit.Assert.*
+
+class GraphicsTest {
+
+    @Test
+    fun testColorSetting() {
+        // TODO: Implement with mocked Canvas
+        assertTrue(true)
+    }
+}
