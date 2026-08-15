33a9f2a8126da9875b00351d52c26f8d7d31fb5a
exu-coder <mdeitu3@gmail.com>
2026-08-15 22:14:14 +0600

    Upload app/src/main/java/com/j2merunner/app/J2MERunnerApp.kt via CLI Uploader

--- app/src/main/java/com/j2merunner/app/J2MERunnerApp.kt
+++ app/src/main/java/com/j2merunner/app/J2MERunnerApp.kt
@@ -1,12 +1,14 @@
 package com.j2merunner.app
 
 import android.app.Application
+import com.j2merunner.engine.midp.RecordStore
 
 class J2MERunnerApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
         instance = this
+        RecordStore.initialize(this)
     }
 
     companion object {
