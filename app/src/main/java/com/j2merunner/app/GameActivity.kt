dd1169c52154805fb8115ad372bce23e2189973a
exu-coder <mdeitu3@gmail.com>
2026-08-15 21:21:44 +0600

    Upload app/src/main/java/com/j2merunner/app/GameActivity.kt via CLI Uploader

---
+++ app/src/main/java/com/j2merunner/app/GameActivity.kt
@@ -0,0 +1,28 @@
+package com.j2merunner.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.j2merunner.app.ui.screens.GamePlayerScreen
+
+class GameActivity : ComponentActivity() {
+
+    companion object {
+        const val EXTRA_JAR_PATH = "jar_path"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val jarPath = intent.getStringExtra(EXTRA_JAR_PATH) ?: return finish()
+
+        setContent {
+            GamePlayerScreen(
+                jarPath = jarPath,
+                onExit = { finish() }
+            )
+        }
+    }
+}
